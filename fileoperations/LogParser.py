bc3f508bb191859303bc28f80d663e81cbe4658d
Your Name <you@example.com>
2021-12-13 10:14:27 +0300

    lai

---
+++ fileoperations/LogParser.py
@@ -0,0 +1,15 @@
+import json
+
+file_name = "log.txt"
+file = open(file_name, "r")
+data = []
+order = ["date", "url", "type", "message"]
+
+for line in file.readlines():
+    details = line.split("|")
+    details = [x.strip() for x in details]
+    structure = {key: value for key, value in zip(order, details)}
+    data.append(structure)
+
+for entry in data:
+    print(json.dumps(entry, indent=4))
